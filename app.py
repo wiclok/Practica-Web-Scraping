154fde63be9691b31eb2a2cd9c854cdf343918a9
Cespedes Brian <cespedes.aguilera.brian@gmail.com>
2024-05-15 10:51:57 -0300

    added function to get all tags hyperlinks

--- app.py
+++ app.py
@@ -3,11 +3,24 @@ from bs4 import BeautifulSoup
 
 def Web_Crawling(url):
   response = requests.get(url)
-  return response
+  
+  return response.text
+
+def extract_all_tags_hyperlinks(web_crawler):
+  soup = BeautifulSoup(web_crawler, 'html.parser')
+  
+  all_tags_hyperlinks = soup.find_all('a')
+  
+  return all_tags_hyperlinks
 
 def Execute_Program():
+  
   url = 'https://books.toscrape.com/'
-  web_crawling = Web_Crawling(url)
+  web_crawler = Web_Crawling(url)
+  
+  all_tag_hyperlink = extract_all_tags_hyperlinks(web_crawler)
+  
+  
   
 
 if __name__ == '__main__':
