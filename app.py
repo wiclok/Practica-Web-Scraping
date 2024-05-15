436730929fcfb3b9ab37fb3988a094cbfb3e446f
Cespedes Brian <cespedes.aguilera.brian@gmail.com>
2024-05-15 10:29:57 -0300

    the function Web_Crawling  return the response.text

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ import requests
 
 def Web_Crawling(url):
   response = requests.get(url)
+  return response
 
 def Execute_Program():
   url = 'https://books.toscrape.com/'
-  Web_Crawling(url)
+  web_crawling = Web_Crawling(url)
+  
 
 if __name__ == '__main__':
   Execute_Program()
